8986ea5e1c6db9fbd5f7b1dd17ccd304ab103a4a
kumar.rahul <kumar.rahul>
2024-01-22 22:21:44 +0530

    Day 5 and Day 6

---
+++ Day-6/hello.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+//#include <conio.h>
+//Q2.
+//Problem Description
+//Print first 5 even numbers i.e. 0, 2, 4, 6, 8.
+//Output Format
+//Print first five even numbers separated by space.
+//Example Output
+//0 2 4 6 8
+void main(){
+//getch();
+//printf("Hello world\n");
+//int a=10;
+//int b = 30;
+//printf("%d %d\n",a,b);
+//printf("A=%d",a);
+//printf("A=%d and B=%d",a*a, a*a*a);
+//printf("0, 2 , 4, 6, 8");
+int a = 0;
+int b = 2;
+int c = 4;
+int d = 6;
+int e = 8;
+printf("%d %d %d %d %d",a,b,c,d,e);
+}
